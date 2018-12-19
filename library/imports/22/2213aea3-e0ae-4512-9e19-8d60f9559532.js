70927cc745549b56dae0ee6949bb6fa8b15a4df6
jimmmmmmmmmyfunnnnnnny <864297397@qq.com>
2018-12-19 18:16:35 +0800

    add png

---
+++ library/imports/22/2213aea3-e0ae-4512-9e19-8d60f9559532.js
@@ -0,0 +1,41 @@
+"use strict";
+cc._RF.push(module, '2213a6j4K5FEp4ZjWD5VZUy', 'goBangGame');
+// Script/goBangGame.js
+
+"use strict";
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        blcakPrefab: cc.Prefab,
+        withePrefab: cc.Prefab,
+        gobangBg: cc.Sprite
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start: function start() {}
+}
+
+// update (dt) {},
+);
+
+cc._RF.pop();
---
+++ temp/quick-scripts/assets/Script/goBangGame.js
@@ -0,0 +1,53 @@
+(function() {"use strict";var __module = CC_EDITOR ? module : {exports:{}};var __filename = 'preview-scripts/assets/Script/goBangGame.js';var __require = CC_EDITOR ? function (request) {return cc.require(request, require);} : function (request) {return cc.require(request, __filename);};function __define (exports, require, module) {"use strict";
+cc._RF.push(module, '2213a6j4K5FEp4ZjWD5VZUy', 'goBangGame', __filename);
+// Script/goBangGame.js
+
+"use strict";
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        blcakPrefab: cc.Prefab,
+        withePrefab: cc.Prefab,
+        gobangBg: cc.Sprite
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start: function start() {}
+}
+
+// update (dt) {},
+);
+
+cc._RF.pop();
+        }
+        if (CC_EDITOR) {
+            __define(__module.exports, __require, __module);
+        }
+        else {
+            cc.registerModuleFunc(__filename, function () {
+                __define(__module.exports, __require, __module);
+            });
+        }
+        })();
+        //# sourceMappingURL=goBangGame.js.map
+        

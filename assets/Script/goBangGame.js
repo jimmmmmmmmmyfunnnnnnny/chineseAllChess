da2d36d3038ca9a219982c3182ffb7bd4080bb78
jimmmmmmmmmyfunnnnnnny <864297397@qq.com>
2019-01-02 18:10:10 +0800

    修改移动旗子

--- assets/Script/goBangGame.js
+++ assets/Script/goBangGame.js
@@ -4,31 +4,67 @@ cc.Class({
     properties: {
         blcakPrefab:cc.Prefab,
         withePrefab:cc.Prefab,
+        gameBg:cc.Sprite,
         gobangBg:cc.Sprite,
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        moveChess:cc.Sprite,
+
     },
-
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {},
-
     start () {
+        console.log(this.moveChess)
+        
+        this.initMap()
+        this.moveChessToMap()
+        
 
     },
+    initMap:function(){
+
+        //(-265,-272), (-228,-272)
+        this.chessMap = []
+        for (let i = 1;i<=15;i++){
+            for(let j = 1; j<=15; j++){
+                let pos = {
+                    x:-265+i*35.4,
+                    y:-272+j*35.4
+                }
+                this.chessMap.push({
+                    pos:pos
+                });
+                console.log("j = "+j+"i"+i)
+            }
+        }
+        console.log( this.chessMap)
+    },
+    setChessMap:function(pos) {
+        for (let i = 0;i<this.chessMap.length; i++){
+            let pos_1 = cc.v2(this.chessMap[i].x, this.chessMap[i].y) 
+            if (pos_1.fuzzyEquals(pos, 15)){
+                let PrefabType = this.blcakPrefab
+                let chess = cc.instantiate(PrefabType)
+                this.gobangBg.node.addChild(chess)
+                chess.position = pos_1
+                    
+                }
+        }
+    },
+    moveChessToMap:function(){
+        this.node.on(cc.Node.EventType.TOUCH_START, function(event){     
+            let touchPos =  this.node.convertToNodeSpaceAR(event.getLocation())
+            this.moveChess.node.position = touchPos
+        }.bind(this), this.node);
+
+        this.node.on(cc.Node.EventType.TOUCH_MOVE, function(event){
+            let touchPos =  this.node.convertToNodeSpaceAR(event.getLocation())
+            this.moveChess.node.position = touchPos
+        }.bind(this), this.node);
+
+        this.node.on(cc.Node.EventType.TOUCH_END, function(event){
+            let touchPos =  this.node.convertToNodeSpaceAR(event.getLocation())
+            this.moveChess.node.position = touchPos
+            this.setChessMap(touchPos)
+            console.log("moveChess pos ="+this.moveChess.node.position);
+        }.bind(this), this.node)
+    }
 
     // update (dt) {},
 });
--- temp/quick-scripts/assets/Script/goBangGame.js
+++ temp/quick-scripts/assets/Script/goBangGame.js
@@ -10,33 +10,68 @@ cc.Class({
     properties: {
         blcakPrefab: cc.Prefab,
         withePrefab: cc.Prefab,
-        gobangBg: cc.Sprite
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        gameBg: cc.Sprite,
+        gobangBg: cc.Sprite,
+        moveChess: cc.Sprite
+
+    },
+    start: function start() {
+        console.log(this.moveChess);
+
+        this.initMap();
+        this.moveChessToMap();
     },
 
-    // LIFE-CYCLE CALLBACKS:
+    initMap: function initMap() {
 
-    // onLoad () {},
+        //(-265,-272), (-228,-272)
+        this.chessMap = [];
+        for (var i = 1; i <= 15; i++) {
+            for (var j = 1; j <= 15; j++) {
+                var pos = {
+                    x: -265 + i * 35.4,
+                    y: -272 + j * 35.4
+                };
+                this.chessMap.push({
+                    pos: pos
+                });
+                console.log("j = " + j + "i" + i);
+            }
+        }
+        console.log(this.chessMap);
+    },
+    setChessMap: function setChessMap(pos) {
+        for (var i = 0; i < this.chessMap.length; i++) {
+            var pos_1 = cc.v2(this.chessMap[i].x, this.chessMap[i].y);
+            if (pos_1.fuzzyEquals(pos, 35.4)) {
+                var PrefabType = this.blcakPrefab;
+                var chess = cc.instantiate(PrefabType);
+                this.gobangBg.node.addChild(chess);
+                chess.position = pos_1;
+            }
+        }
+    },
+    moveChessToMap: function moveChessToMap() {
+        this.node.on(cc.Node.EventType.TOUCH_START, function (event) {
+            var touchPos = this.node.convertToNodeSpaceAR(event.getLocation());
+            this.moveChess.node.position = touchPos;
+        }.bind(this), this.node);
 
-    start: function start() {}
-}
+        this.node.on(cc.Node.EventType.TOUCH_MOVE, function (event) {
+            var touchPos = this.node.convertToNodeSpaceAR(event.getLocation());
+            this.moveChess.node.position = touchPos;
+        }.bind(this), this.node);
 
-// update (dt) {},
-);
+        this.node.on(cc.Node.EventType.TOUCH_END, function (event) {
+            var touchPos = this.node.convertToNodeSpaceAR(event.getLocation());
+            this.moveChess.node.position = touchPos;
+            this.setChessMap(touchPos);
+            console.log("moveChess pos =" + this.moveChess.node.position);
+        }.bind(this), this.node);
+    }
+
+    // update (dt) {},
+});
 
 cc._RF.pop();
         }
